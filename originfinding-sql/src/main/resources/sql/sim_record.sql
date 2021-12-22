ad5d6f1dd14d73062d5360ce21f149d7fd89b69a
includeno <includeno@126.com>
2021-12-22 15:58:40 +0800

    bugfix 初始化数据库sql脚本

--- originfinding-sql/src/main/resources/sql/sim_record.sql
+++ originfinding-sql/src/main/resources/sql/sim_record.sql
@@ -14,7 +14,7 @@
 --
 -- Table structure for table `sim_record`
 --
-
+Use demo;
 DROP TABLE IF EXISTS `sim_record`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
