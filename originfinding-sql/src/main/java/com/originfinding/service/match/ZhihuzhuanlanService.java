2dcb586b60094a3d293bd4f5e75e7b0d4579916b
includeno <includeno@126.com>
2021-12-31 11:26:34 +0800

    bugfix修正url匹配问题

--- originfinding-sql/src/main/java/com/originfinding/service/match/ZhihuzhuanlanService.java
+++ originfinding-sql/src/main/java/com/originfinding/service/match/ZhihuzhuanlanService.java
@@ -8,19 +8,19 @@ import java.util.regex.Pattern;
 @Service
 @Slf4j
 public class ZhihuzhuanlanService implements MatchService {
-    public static final String pattern="https://zhuanlan.zhihu.com/p/(.*)";
-    public static final Pattern re = Pattern.compile(pattern);
-    public static final String host="zhuanlan.zhihu.com";
+    public static final String[] patterns = new String[]{
+            "https://zhuanlan.zhihu.com/p/(.*)",//https://zhuanlan.zhihu.com/p/88403925
+    };
 
     @Override
     public boolean match(String url) {
-        String cur=getHost(url);
-        if(cur.equals(host)&&re.matcher(url).matches()){
-            return true;
+        for (String pattern : patterns) {
+            Pattern p = Pattern.compile(pattern);
+            if (p.matcher(url).matches()) {
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
     }
 }
 
