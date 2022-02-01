ab3960d1d30f76229fe33ae1bd59802698b3cce5
includeno <includeno@126.com>
2022-02-01 18:06:18 +0800

    bugfix sparklda topic名字错误

--- originfinding-sql/src/main/java/com/originfinding/config/KafkaTopic.java
+++ originfinding-sql/src/main/java/com/originfinding/config/KafkaTopic.java
@@ -14,7 +14,7 @@ public class KafkaTopic {
     public static final String sparkresult="sparkresult";//Spark 结果
 
     public static final String updateRedis ="updateRedis";//跳过爬虫部分直接获取最新爬取结果进行Spark lda步骤
-    public static final String submitsparklda ="submitsparklda";//Spark lda步骤处理 提交
+    public static final String sparklda ="sparklda";//Spark lda步骤处理 提交
     public static final String sparkPairAnalyze="sparkPairAnalyze";//Spark 分析原创文章和非原创文章配对任务
     public static final String sparkPairAnalyzeResult="sparkPairAnalyzeResult";//Spark 分析原创文章和非原创文章配对 处理结果
 
@@ -27,7 +27,7 @@ public class KafkaTopic {
     }
     @Bean
     public NewTopic sparklda() {
-        return TopicBuilder.name(KafkaTopic.submitsparklda)
+        return TopicBuilder.name(KafkaTopic.sparklda)
                 .partitions(12)
                 .replicas(1)
                 .build();
